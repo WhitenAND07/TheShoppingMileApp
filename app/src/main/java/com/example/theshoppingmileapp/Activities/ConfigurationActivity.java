ce6a47e584375ede94a0f514bace22e99bd26c34
Jordi Blanco Lopez <jordi14and@gmail.com>
2019-03-20 18:20:51 +0100

    Add more activities

---
+++ app/src/main/java/com/example/theshoppingmileapp/Activities/ConfigurationActivity.java
@@ -0,0 +1,16 @@
+package com.example.theshoppingmileapp.Activities;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.example.theshoppingmileapp.R;
+
+public class ConfigurationActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuration);
+    }
+
+}
--- app/src/main/java/com/example/theshoppingmileapp/Activities/MainActivity.java
+++ app/src/main/java/com/example/theshoppingmileapp/Activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.theshoppingmileapp;
+package com.example.theshoppingmileapp.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.theshoppingmileapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
